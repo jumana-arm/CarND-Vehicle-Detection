1689fdebe3686999c9349530edf1ee019313ea9f
jumana-arm <jumana.mp@arm.com>
2018-01-18 19:12:05 -0600

    Adds a notebook for sliding window experiments

--- vehicle_detection/sliding_window.py
+++ vehicle_detection/sliding_window.py
@@ -40,16 +40,16 @@ def detect_cars(img, ystart, ystop, scale, svc, cspace, hog_channel, orient, pix
         ch1 = ctrans_tosearch[:,:,hog_channel]
 
     # Define blocks and steps as above
-    nxblocks = (ch1.shape[1] // pix_per_cell)+ 1
-    nyblocks = (ch1.shape[0] // pix_per_cell)+ 1 
+    nxblocks = (ch1.shape[1] // pix_per_cell) - cell_per_block + 1
+    nyblocks = (ch1.shape[0] // pix_per_cell) - cell_per_block + 1 
     nfeat_per_block = orient*cell_per_block**2
     
     # 64 was the orginal sampling rate, with 8 cells and 8 pix per cell
     window = 64
-    nblocks_per_window = (window // pix_per_cell)+ 1
+    nblocks_per_window = (window // pix_per_cell) - cell_per_block + 1
     cells_per_step = 2  # Instead of overlap, define how many cells to step
-    nxsteps = (nxblocks - nblocks_per_window) // cells_per_step
-    nysteps = (nyblocks - nblocks_per_window) // cells_per_step
+    nxsteps = (nxblocks - nblocks_per_window) // cells_per_step + 1
+    nysteps = (nyblocks - nblocks_per_window) // cells_per_step + 1
     
     # Compute individual channel HOG features for the entire image
     hog1 = feature.get_hog_features(ch1, orient, pix_per_cell, cell_per_block, feature_vec=False)
@@ -72,6 +72,8 @@ def detect_cars(img, ystart, ystop, scale, svc, cspace, hog_channel, orient, pix
                 hog_features = np.hstack((hog_feat1, hog_feat2, hog_feat3))
             else:
                 hog_features = hog_feat1
+                
+            hog_features =  hog_features.reshape(1, -1)
 
             xleft = xpos*pix_per_cell
             ytop = ypos*pix_per_cell
@@ -84,9 +86,7 @@ def detect_cars(img, ystart, ystop, scale, svc, cspace, hog_channel, orient, pix
                 win_draw = np.int(window*scale)
                 car_windows.append(((xbox_left, ytop_draw + ystart),(xbox_left + win_draw, ytop_draw + win_draw + ystart)))               
     return car_windows
-    
-   
-# Here is your draw_boxes function from the previous exercise
+
 def draw_boxes(img, bboxes, color=(0, 0, 255), thick=6):
     # Make a copy of the image
     imcopy = np.copy(img)
@@ -128,102 +128,3 @@ def draw_labeled_bboxes(img, labels):
         cv2.rectangle(img, bbox[0], bbox[1], (0,0,255), 6)
     # Return the image
     return img
-
-# Define a single function that can extract features using hog sub-sampling and make predictions
-def find_cars(img, ystart, ystop, scale, cspace, hog_channel, svc, orient, 
-              pix_per_cell, cell_per_block):
-    
-    # array of rectangles where cars were detected
-    rectangles = []
-    
-    img = img.astype(np.float32)/255
-    
-    img_tosearch = img[ystart:ystop,:,:]
-
-    # apply color conversion if other than 'RGB'
-    if cspace != 'RGB':
-        if cspace == 'HSV':
-            ctrans_tosearch = cv2.cvtColor(img_tosearch, cv2.COLOR_RGB2HSV)
-        elif cspace == 'LUV':
-            ctrans_tosearch = cv2.cvtColor(img_tosearch, cv2.COLOR_RGB2LUV)
-        elif cspace == 'HLS':
-            ctrans_tosearch = cv2.cvtColor(img_tosearch, cv2.COLOR_RGB2HLS)
-        elif cspace == 'YUV':
-            ctrans_tosearch = cv2.cvtColor(img_tosearch, cv2.COLOR_RGB2YUV)
-        elif cspace == 'YCrCb':
-            ctrans_tosearch = cv2.cvtColor(img_tosearch, cv2.COLOR_RGB2YCrCb)
-    else: ctrans_tosearch = np.copy(image)   
-    
-    # rescale image if other than 1.0 scale
-    if scale != 1:
-        imshape = ctrans_tosearch.shape
-        ctrans_tosearch = cv2.resize(ctrans_tosearch, (np.int(imshape[1]/scale), np.int(imshape[0]/scale)))
-    
-    # select colorspace channel for HOG 
-    if hog_channel == 'ALL':
-        ch1 = ctrans_tosearch[:,:,0]
-        ch2 = ctrans_tosearch[:,:,1]
-        ch3 = ctrans_tosearch[:,:,2]
-    else: 
-        ch1 = ctrans_tosearch[:,:,hog_channel]
-
-    # Define blocks and steps as above
-    nxblocks = (ch1.shape[1] // pix_per_cell)+1  #-1
-    nyblocks = (ch1.shape[0] // pix_per_cell)+1  #-1 
-    nfeat_per_block = orient*cell_per_block**2
-    # 64 was the orginal sampling rate, with 8 cells and 8 pix per cell
-    window = 64
-    nblocks_per_window = (window // pix_per_cell)-1 
-    cells_per_step = 2  # Instead of overlap, define how many cells to step
-    nxsteps = (nxblocks - nblocks_per_window) // cells_per_step
-    nysteps = (nyblocks - nblocks_per_window) // cells_per_step
-    
-    # Compute individual channel HOG features for the entire image
-    hog1 = feature.get_hog_features(ch1, orient, pix_per_cell, cell_per_block, feature_vec=False)   
-    if hog_channel == 'ALL':
-        hog2 = feature.get_hog_features(ch2, orient, pix_per_cell, cell_per_block, feature_vec=False)
-        hog3 = feature.get_hog_features(ch3, orient, pix_per_cell, cell_per_block, feature_vec=False)
-    
-    for xb in range(nxsteps):
-        for yb in range(nysteps):
-            ypos = yb*cells_per_step
-            xpos = xb*cells_per_step
-            # Extract HOG for this patch
-            hog_feat1 = hog1[ypos:ypos+nblocks_per_window, xpos:xpos+nblocks_per_window].ravel()
-            if hog_channel == 'ALL':
-                hog_feat2 = hog2[ypos:ypos+nblocks_per_window, xpos:xpos+nblocks_per_window].ravel() 
-                hog_feat3 = hog3[ypos:ypos+nblocks_per_window, xpos:xpos+nblocks_per_window].ravel() 
-                hog_features = np.hstack((hog_feat1, hog_feat2, hog_feat3)).reshape(1, -1)   
-            else:
-                hog_features = hog_feat1
-
-            xleft = xpos*pix_per_cell
-            ytop = ypos*pix_per_cell
-            
-            ################ ONLY FOR BIN_SPATIAL AND COLOR_HIST ################
-
-            # Extract the image patch
-            #subimg = cv2.resize(ctrans_tosearch[ytop:ytop+window, xleft:xleft+window], (64,64))
-          
-            # Get color features
-            #spatial_features = bin_spatial(subimg, size=spatial_size)
-            #hist_features = color_hist(subimg, nbins=hist_bins)
-
-            # Scale features and make a prediction
-            #test_features = X_scaler.transform(np.hstack((spatial_features, hist_features, hog_features)).reshape(1, -1))    
-            #test_features = X_scaler.transform(np.hstack((shape_feat, hist_feat)).reshape(1, -1))    
-            #test_prediction = svc.predict(test_features)
-            
-            ######################################################################
-            
-            test_prediction = svc.predict(hog_features)
-            
-            if test_prediction == 1 or show_all_rectangles:
-                xbox_left = np.int(xleft*scale)
-                ytop_draw = np.int(ytop*scale)
-                win_draw = np.int(window*scale)
-                rectangles.append(((xbox_left, ytop_draw+ystart),(xbox_left+win_draw,ytop_draw+win_draw+ystart)))
-                
-    return rectangles
-    
-print('...')
